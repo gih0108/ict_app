5f9585a5173e2a40608cfd571f808b9fb10db4b0
student29 <student29@student29.local>
2018-05-09 19:55:58 +0900

    g

---
+++ Match App/CardCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CardCollectionViewCell.swift
+//  Match App
+//
+//  Created by student35 on 2018. 5. 3..
+//  Copyright © 2018년 student35. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var frontImageView: UIImageView!
+    
+    @IBOutlet weak var backImageView: UIImageView!
+    
+}
