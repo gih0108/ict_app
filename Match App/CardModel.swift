afdab3bee07ac6f1693150aaaf70d43f388bb46c
student29 <student29@student29.local>
2018-05-10 20:51:15 +0900

    th

--- Match App/CardModel.swift
+++ Match App/CardModel.swift
@@ -15,10 +15,12 @@ class CardModel {
         
         // Declare an array to store the generated cards
         var generatedCardsArray = [Card]()
+    
         
         // Randomly generate pairs of cards
         for _ in 1...8 {
             
+            //Get a random number
             let randomNumber = arc4random_uniform(13) + 1
             
             // Log the number
@@ -28,6 +30,7 @@ class CardModel {
             let cardOne = Card()
             cardOne.imageName = "card\(randomNumber)"
             
+            // Add the created card
             generatedCardsArray.append(cardOne)
             
             // Create the second card object
@@ -37,7 +40,6 @@ class CardModel {
             generatedCardsArray.append(cardTwo)
             
             // OPTIONAL : Make it so we only have unique pairs of cards
-            
         }
         
         // TODO : Randomize the array
---
+++ Match App/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Match App
+//
+//  Created by student29 on 2018. 5. 10..
+//  Copyright © 2018년 student35. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
