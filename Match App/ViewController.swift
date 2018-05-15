bf98e908520aa490ef75e985f7deb6fd3db9f5eb
student29 <21600555@handong.edu>
2018-05-15 20:19:02 +0900

    20180515

--- Match App/ViewController.swift
+++ Match App/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     var model = CardModel()
     var cardArray = [Card]()
     
+    var firstFlippedCard:IndexPath?
+    
     var firstFlippedCardIndex:IndexPath?
 
     override func viewDidLoad() {
@@ -59,18 +61,33 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+        // Get the cell that the user selected
         let cell = collectionView.cellForItem(at: indexPath) as! CardCollectionViewCell
         
+        // Get the card that the user selected
         let card = cardArray[indexPath.row]
         
         if card.isFlipped == false && card.isMatched == false {
+            
+            // Flip the card
             cell.flip()
             
+            // Set the status of the card
             card.isFlipped = true
+            
+            // Determine if it's the first card or second card that's flipped over
             
             
             if firstFlippedCardIndex == nil {
+                // This is the first card being flipped
+                firstFlippedCard = indexPath
                 
+            }else{
+                // Flip the card back
+                cell.flipBack()
+                
+                // Set the status of the card
+                card.isFlipped = false
             }
         }
         
