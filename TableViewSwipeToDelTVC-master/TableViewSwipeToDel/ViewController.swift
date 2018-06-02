6d3e2b762f5bf4a518af07eac12940118c86a89a
gih0108 <31753310+gih0108@users.noreply.github.com>
2018-06-02 23:27:42 +0900

    Add files via upload

---
+++ TableViewSwipeToDelTVC-master/TableViewSwipeToDel/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  TableViewSwipeToDel
+//
+//  Created by BriefOS on 2/16/2560 BE.
+//  Copyright © 2560 BriefOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var titles = ["Title 1", "Title 2", "Title 3", "Title 4", "Title 5", "Title 6", "Title 7", "Title 8", "Title 9", "Title 10"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "titleCell", for: indexPath)
+        cell.textLabel?.text = titles[indexPath.item]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            titles.remove(at: indexPath.item)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+
+
+}
+
